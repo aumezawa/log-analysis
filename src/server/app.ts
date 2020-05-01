cd9499d0f41a874954550378f76d8577d3717589
aumezawa <letzt.apostel@gmail.com>
2020-05-01 14:07:36 +0900

    Access logging enabled

--- src/server/app.ts
+++ src/server/app.ts
@@ -2,6 +2,9 @@ import * as express from "express"
 import * as bodyParser from "body-parser"
 import * as path from "path"
 import * as helmet from "helmet"
+import * as log4js from "log4js"
+
+import logger = require("./lib/logger")
 
 import indexRouter from "./routes/index"
 import apiV1Router from "./routes/api-v1"
@@ -14,6 +17,12 @@ app.set("http_port", 3000)
 app.set("https_port", 3443)
 app.set("tokenKey", process.env.npm_package_name)
 
+app.use(log4js.connectLogger(logger, {
+  level: "auto",
+  format: (req: any, res: any, formatter: ((str: string) => string)) => (
+    formatter(`${ req.protocol.toUpperCase() }/:http-version :method :status [${ (req.token && req.token.usr) || "unknown" } (:remote-addr)] :url - :user-agent - :response-time ms`)
+  )
+}))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(helmet())
